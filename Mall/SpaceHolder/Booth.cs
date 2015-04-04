fc43a56a5778954fee4c62dd6330cea700a45f2b
pkeremidchieva <pkeremidchieva@users.noreply.github.com>
2015-04-04 18:30:39 +0300

    Update Booth.cs

--- Mall/SpaceHolder/Booth.cs
+++ Mall/SpaceHolder/Booth.cs
@@ -1,18 +1,39 @@
-﻿namespace Mall.Staff
+namespace Mall.Staff
 {
+    using Mall.SpaceHolder;
     using System;
+    using System.Object; 
+    
 
-    public class Booth
+    public class Booth 
     {
         //Fields
-
+        private Floors floor;
+        private double areaRentalPrice;
+        
         //Constructors
+        public Booth(Floors floors, double areaRentalPrice)
+        {
+            this.Floor = floor;
+            this.AreaRentalPrice = areaRentalPrice;
+        }
+
+        //Properties
+        public Floors Floor
+        {
+            get { return this.floor; }
+            private set { this.floor = value; }
+        }
+
+        public double AreaRentalPrice
+        {
+            get { return areaRentalPrice; }
+            set { areaRentalPrice = value; }
+        }
 
         //Enums
 
         //Interfaces
-
-        //Properties
 
         //Methods
 
