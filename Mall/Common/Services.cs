63109a67b572b9c1b2a68e2e0d7f5acec9269d48
pkeremidchieva <p.keremidchieva@gmail.com>
2015-04-07 20:40:43 +0300

    change
    Services addes

---
+++ Mall/Common/Services.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Mall.Common
+{
+    public enum Services
+    {
+        
+
+         Haircut = 0,
+         Manicures = 1, 
+         Massage = 2,
+         ChocolateTherapy = 3,
+         SkinCleaning = 4,
+         SkinTanning = 5,
+         Waxing = 6, 
+         BikiniWax = 7, 
+         Pedicures = 8, 
+         MudBath = 9 
+
+    }
+}
