e3b7f1e8fb6b6194700fb7ebcf336e4b6c3ab6b2
pkeremidchieva <p.keremidchieva@gmail.com>
2015-04-07 20:42:02 +0300

    Add
    Foods

---
+++ Mall/Common/Foods.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Mall.Common
+{
+    public enum  Foods
+    {
+        Bread = 0,
+        Milk = 1,
+        Rice = 2,
+        Potatoes = 3,
+        Mushrooms = 4,
+        Cheese = 5,
+        Carrots = 6,
+        Oranges = 7,
+        Cucumbers = 8,
+        OrangeJuice = 9,
+        Meat = 10,
+        Pepper = 11,
+        Beef = 12,
+        WaterMelon = 13,
+        Cherries = 14,
+        Apples = 15,
+        Lamp = 16,
+        Pumpkin = 17,
+        Honey = 18
+    }
+}
